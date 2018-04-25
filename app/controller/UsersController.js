7389db0f6aaf7ce1d5cc5650856fc2e11e3bf36d
bdbipbip <baptiste.dubarry21@gmail.com>
2018-04-25 11:02:21 +0200

    Complete CRUD on users

--- app/controller/UsersController.js
+++ app/controller/UsersController.js
@@ -6,70 +6,101 @@ var utilisateur = require("../models/utilisateur");
 
 
 // middleware to use for all requests
-router.use(function(req, res, next) {
-    console.log('Router middleware log, request : ', req.url); // do logging
-    next(); // make sure we go to the next routes and don't stop here
+router.use(function (req, res, next) {
+  console.log('Router middleware log, request : ', req.url); // do logging
+  next(); // make sure we go to the next routes and don't stop here
 });
 
- 
+
 // ROUTES FOR USERS
 // =============================================================================
 
 // create a new user
 router.post('/', function (req, res) {
 
-  var newUser = new utilisateur();
+  utilisateur.findOne({ 'mail': req.body.mail }, function (err, user) {
+    if (err) return res.status(500).send("There was a problem finding users in db");
+    else if (user) return res.status(500).send("This email adress is already used");
+    else {
+      var newUser = new utilisateur();
+      var idAssoc = mongoose.Types.ObjectId(req.body.idAssoc);
+      // set the user's local credentials
+      newUser.mail = req.body.mail;
+      newUser.motDePasse = newUser.generateHash(req.body.motDePasse);
+      newUser.nom = req.body.nom;
+      newUser.adresse = req.body.adresse;
+      newUser.dateNaissance = req.body.dateNaissance;
+      newUser.sexe = req.body.sexe;
+      newUser.idAssoc = idAssoc;
 
-  // set the user's local credentials
-  newUser.mail    = req.body.mail;
-  newUser.motDePasse = newUser.generateHash(req.body.motDePasse);
-  newUser.nom = req.body.nom;
-  newUser.adresse = req.body.adresse;
-  newUser.dateNaissance = req.body.dateNaissance;
-  newUser.sexe = req.body.sexe;
-
-  console.log("New User : "+newUser);
-
-  // save the user
-  newUser.save(function (err, utilisateur) {
-    if (err){
-    return res.status(500).send("There was a problem adding infos to db");
-  } 
-    console.log("User created ", utilisateur);
-    res.status(200).send(utilisateur.nom+" has been created");
+      // save the user
+      newUser.save(function (err, utilisateur) {
+        if (err) {
+          return res.status(500).send("There was a problem adding infos to db");
+        }
+        console.log("User created ", utilisateur);
+        var username = utilisateur.nom || "No name";
+        var resMsg = `${username} has been created ans his id is : ${utilisateur.id}`;
+        res.status(200).send(resMsg);
+      });
+    }
   });
+
+
 });
 
 // get the list of all users
 router.get('/', function (req, res) {
-  console.log("HELLO IM TRYING TO GET USERS")
   utilisateur.find({}, function (err, utilisateurs) {
     if (err) return res.status(500).send("There was a problem finding users in db");
     res.status(200).send(utilisateurs);
   });
 });
 
-/* WILL BE IMPLEMENTED SOON
-router.post('/', function (req, res) {
-  User.create({
-    username: req.body.username,
-    password: req.body.password,
-    association: req.body.association
-  },
-  function (err, user) {
-    if (err) return res.status(500).send("There was a problem adding infos to db");
+// get the user by Id
+router.get('/:id', function (req, res) {
+  var userId = req.params.id;
+  utilisateur.findById(userId, function (err, user) {
+    if (err) return res.status(500).send("Wrong ID, no user found");
     res.status(200).send(user);
   });
 });
 
-router.get('/:username', function (req, res) {
-  User.find({ 'username': req.params.username }, function(err, users) {
+// Get user by mail
+router.get('/mail/:mail', function (req, res) {
+  utilisateur.findOne({ 'mail': req.params.mail }, function (err, user) {
     if (err) return res.status(500).send("There was a problem finding users in db");
-    res.status(200).send(users);
+    res.status(200).send(user);
   });
 });
-*/
 
+router.put('/:id', function (req, res) {
+  var userId = req.params.id;
+  if (req.body.idAssoc) {
+    var newIdAssoc = mongoose.Types.ObjectId(req.body.idAssoc);
+    utilisateur.findByIdAndUpdate(userId, { idAssoc: newIdAssoc }, function (err, user) {
+      if (err || !user) return res.status(500).send("Unable to modify user's assoc");
+      return utilisateur.findById(userId, (err, updatedUser) => res.status(200).send("Updated User (asso) : "+updatedUser));
+    });
+  } else {
+    console.log("Change the whole user");
+    utilisateur.findByIdAndUpdate(userId,
+    {
+      nom : req.body.nom,
+      adresse: req.body.adresse,
+      mail: req.body.mail,
+      dateNaissance: req.body.dateNaissance,
+      sexe: req.body.sexe
+    }, function (err, user) {
+      if (err || !user) return res.status(500).send("Unable to modify the whole user");
+      return utilisateur.findById(userId, (err, updatedUser) => res.status(200).send("Updated User (whole) : "+updatedUser));
+    });
+  }
+  });
 
+router.delete('/:id', function (req, res) {
+  var userId = req.params.id;
+  return utilisateur.findByIdAndRemove(userId, (err) => err ? res.status(500).send("Unable to delete the user") : res.status(200).send("User deleted"));
+});
 
 module.exports = router;
