8f976dcc67dc02bf6a8f463e61e4e554c1c4afa7
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2018-04-26 11:10:12 +0200

    Implemented python code for IR sensor

---
+++ sensor/utils.py
@@ -0,0 +1,27 @@
+import threading
+import time
+
+__all__ = ['periodic_timer']
+
+
+class periodic_timer:
+    def __init__(self, func, period=1., args=[], kwargs={}):
+        self.func = func
+        self.period = period
+        self._args = args
+        self._kwargs = kwargs
+
+    def stop(self):
+        if self._timer is not None:
+            self._timer.cancel()
+
+    def start(self, count=-1):
+        if count != 0:
+            start = time.time()
+            self.func(*self._args, **self._kwargs)
+            delta_t = time.time() - start
+            if delta_t < self.period:
+                self._timer = threading.Timer(self.period - delta_t, self.start, args=[count - 1])
+                self._timer.start()
+            else:
+                self.start(count - 1)
