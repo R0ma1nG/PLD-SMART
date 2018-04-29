61453d1050f026db27ec15c4b1d78eb19bfbb5a5
Paul-Emmanuel SOTIR <paul-emmanuel@outlook.com>
2018-04-29 21:16:49 +0200

    Implemented populate_db.js script

--- app/models/poubelle.js
+++ app/models/poubelle.js
@@ -1,9 +1,17 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-var PoubelleSchema   = new Schema({
+var PoubelleSchema = new Schema({
     _id: Schema.Types.ObjectId,
+    id_grandlyon: String,
     adresse: String,
+    code_insee: Number,
+    code_postal: Number,
+    commune: String,
+    gestionnaire: String,
+    observation: String,
+    numero_voie: String,
+    voie: String,
     lattitude: Number,
     longitude: Number,
     remplissage: Number
