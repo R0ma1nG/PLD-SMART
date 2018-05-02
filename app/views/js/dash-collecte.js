42e00d67cd989a5428b135ac445f3a0abbefb2cc
Romain <rom.gouttefangeas@gmail.com>
2018-05-02 15:04:14 +0200

    refactoring

--- app/views/js/dash-collecte.js
+++ app/views/js/dash-collecte.js
@@ -17,3 +17,40 @@ function dateChanged(e){
             });*/
     });
 }
+
+function initMap() {
+    var lyon = { lat: 45.75, lng: 4.85 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: lyon
+    });
+    var marker = new google.maps.Marker({
+        position: lyon,
+        map: map
+    });
+}
+
+function init() {
+    feather.replace();
+    document.getElementById('datePicker').valueAsDate = new Date();
+    var url = "/api/admin/releves/";
+    /*$.ajax 
+        ({
+            type: "GET",
+            contentType: "application/json",
+            //the url where you want to sent the userName and password to
+            url: `${url}`,
+            dataType: 'json',
+            error: function (err) {
+                alert('An error occured !');
+            }
+        })
+        .done(function (data) {
+            data.forEach(function (element) {
+                var contenu = '<tr>';
+                contenu += '<td>'+element._id+'</td><td>'+element.date+'</td><td>'+element.tauxRemplissage+'</td>';
+                contenu += '</tr>';
+                $("#recap").append(contenu);
+            });
+        });*/
+}
