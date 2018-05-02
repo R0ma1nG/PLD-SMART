377d9d53aef9b7ff3f077ce26589d7efe4325a60
Romain <rom.gouttefangeas@gmail.com>
2018-05-02 09:38:02 +0200

    ajout routes statiques pour css

--- app/app.js
+++ app/app.js
@@ -16,6 +16,8 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(express.static('views'));
 
 // Passport required
 app.use(session({ secret: 'smart' })); // session secret
